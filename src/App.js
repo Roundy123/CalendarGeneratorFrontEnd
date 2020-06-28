dc3d2565a8f04ce33497413fa5b53af239967a27
Adam Round <adamround@hotmail.com>
2020-06-28 11:11:29 +0100

    adding total api calls number

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { Button, Spinner } from "reactstrap";
+import { Button, Spinner, Container, Row, Col } from "reactstrap";
 import axios from "axios";
 import { CSVLink, CSVDownload } from "react-csv";
 
 // todo - add fancier date range picker
 // todo - default end date to a week in the future
+// todo - show http errors on website e.g. end date before start date
 
 class App extends React.Component {
   state = {
@@ -17,7 +18,12 @@ class App extends React.Component {
     data: [],
     spinnerDisplay: "none",
     downloadDisplay: "none",
+    totalAPICalls: [],
   };
+
+  componentDidMount() {
+    this.getTotalAPICalls();
+  }
 
   handleStartDateChange = (date) => {
     this.setState({
@@ -42,8 +48,10 @@ class App extends React.Component {
     this.setState({ spinnerDisplay: "inline-block" });
     const startDate = this.dateToString(this.state.startDate);
     const endDate = this.dateToString(this.state.endDate);
+    this.updateTotalAPICalls();
     axios
       .get(
+        // Originially I tried calling the API in AWS but it's not sercure on HTTPS so Heroku didn't like it. As a quick workaround I just deployed a copy of the API in Heroku.
         `https://calendar-generator-api.herokuapp.com/calendar?start_date=${startDate}&end_date=${endDate}`
       )
       .then((res) => {
@@ -54,39 +62,78 @@ class App extends React.Component {
       .catch((err) => console.log(err));
   };
 
+  getTotalAPICalls = () => {
+    axios
+      .get("https://guarded-bayou-83935.herokuapp.com/getapicalls")
+      .then((res) => {
+        this.setState({ totalAPICalls: res.data.apiCalls });
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  updateTotalAPICalls = () => {
+    axios
+      .post("https://guarded-bayou-83935.herokuapp.com/updateapicalls")
+      .then((res) => {
+        this.setState({ totalAPICalls: res.data.apiCalls });
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+  };
+
   render() {
     return (
       <div className="App">
-        hello
-        <br />
-        <DatePicker
-          selected={this.state.startDate}
-          onChange={this.handleStartDateChange}
-        />
-        <br />
-        <DatePicker
-          selected={this.state.endDate}
-          onChange={this.handleEndDateChange}
-        />
-        <br />
-        <Button onClick={this.handleClick}>Get Calendar</Button>{" "}
-        <Spinner
-          type="grow"
-          color="info"
-          style={{
-            width: "3rem",
-            height: "3rem",
-            display: this.state.spinnerDisplay,
-          }}
-        />
-        <CSVLink
-          data={this.state.data}
-          style={{
-            display: this.state.downloadDisplay,
-          }}
-        >
-          Download CSV
-        </CSVLink>
+        <Container className="themed-container" fluid={true}>
+          <Row>
+            <Col
+              xs="3"
+              className="border shadow p-5"
+              style={{ borderRadius: "10px" }}
+            >
+              <br />
+              <DatePicker
+                selected={this.state.startDate}
+                onChange={this.handleStartDateChange}
+              />
+              <br />
+              <DatePicker
+                selected={this.state.endDate}
+                onChange={this.handleEndDateChange}
+              />
+              <br />
+              <br />
+              <Button onClick={this.handleClick}>Get Calendar</Button>{" "}
+              <Spinner
+                type="grow"
+                color="info"
+                style={{
+                  width: "3rem",
+                  height: "3rem",
+                  display: this.state.spinnerDisplay,
+                }}
+              />
+              <CSVLink
+                data={this.state.data}
+                style={{
+                  display: this.state.downloadDisplay,
+                }}
+              >
+                Download CSV
+              </CSVLink>
+            </Col>
+            <Col
+              xs="2"
+              className="border shadow p-5 number"
+              style={{ borderRadius: "10px", margin: "10px" }}
+            >
+              Total API Calls
+              <br />
+              {this.state.totalAPICalls}
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
